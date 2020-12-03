a253bee01a0f0c06ba25710ad057280f2214b1d0
TAGdup31415 <58273949+TAGdup31415@users.noreply.github.com>
2020-12-03 16:01:15 -0500

    Added reading of season input, file path needs work

--- DreamDraft/MaxHeapSolution.cpp
+++ DreamDraft/MaxHeapSolution.cpp
@@ -1,48 +1,101 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 #include <unordered_map>
 using namespace std;
 
 struct Player {										// Player object 
-	string name;									// Player name
-	int scores[18];									// array of scores, index 0 stores total points for the season, 1-17 store corresponding weekly scores
-	int oneC, twoC = 0;								// complement value (points to be gained through substitution) to the starter, twoC used only for RB2 and WR2
+	string name;
+	double scores[18];									// array of scores, index 0 stores total points for the season, 1-17 store corresponding weekly scores
+	double oneC, twoC = 0;								// complement value (points to be gained through substitution) to the starter, twoC used only for RB2 and WR2
+
+	Player(string name, double total) {
+		this->name = name;
+		scores[0] = total;
+	}
 	Player() {
 		name = "";
 	}
-	Player(string name) {
-		this->name = name;
-	}
 };
 
-Player* findComplement(vector<Player>& players, Player* starter) {
+/*Player* findComplement(vector<Player>& players, Player* starter) {
 
 }
 
 void printStats() {
 
-}
+}*/
 
 int main() {
-	vector<Player> QBs, RBs, WRs, TEs, DSTs, Ks;
-	Player *QB, *RB1, *RB2, *WR1, *WR2, *TE, *DST, *K;
-	Player *QBc, *RB1c, *RB2c, *WR1c, *WR2c, *TEc, *DSTc, *Kc;
+	unordered_map<string, Player> QBs, RBs, WRs, TEs;		// map from player name to their object allows for O(1) lookup by name
+	Player *QB = nullptr, *RB1 = nullptr, *RB2 = nullptr, *WR1 = nullptr, *WR2 = nullptr, *TE = nullptr;				// starters
+	Player *QBc, *RB1c, *RB2c, *WR1c, *WR2c, *TEc;			// complements
 
 	// open season file
+	string season, temp, name, pos, score;
 	cout << "enter the season you would like to compute the perfect roster for: ";
-	for () {// read in season stats, record starters
+	cin >> season;
+	string filename = "data_v2/yearly/ " + season + ".csv";	// TODO - fix file path so that it opens
+	cout << filename << endl;
+	ifstream seasonStats(filename);
+	if (seasonStats.is_open())
+		cout << "file opened successfully" << endl;
+	else
+		cout << "file not opened" << endl;
 
+	while (getline(seasonStats, temp, ',')) {				// read in season stats, record starters
+		getline(seasonStats, name, ',');					// reads in player name				
+		getline(seasonStats, temp, ',');
+		getline(seasonStats, pos, ',');						// reads in position
+		for (int i = 0; i < 23; i++) { getline(seasonStats, temp, ','); }
+		getline(seasonStats, score);						// reads total season score, the last column
+		double seasonTotal = stod(score);
+		if (pos == "QB") {
+			QBs[name] = Player(name, seasonTotal);
+			if (QB == nullptr || seasonTotal > QB->scores[0])
+				QB = &QBs[name];
+		}
+		else if (pos == "RB") {
+			RBs[name] = Player(name, seasonTotal);
+			if (seasonTotal > RB1->scores[0] || RB1 == nullptr) {
+				RB2 = RB1;
+				RB1 = &RBs[name];
+			}
+			else if (seasonTotal > RB2->scores[0] || RB2 == nullptr) 
+				RB2 = &RBs[name];
+		}
+		else if (pos == "WR") {
+			WRs[name] = Player(name, seasonTotal);
+			if (seasonTotal > WR1->scores[0] || WR1 == nullptr) {
+				WR2 = WR1;
+				WR1 = &WRs[name];
+			}
+			else if (seasonTotal > WR2->scores[0] || WR2 == nullptr) 
+				WR2 = &WRs[name];
+		}
+		else if (pos == "TE") {
+			TEs[name] = Player(name, seasonTotal);
+			if (seasonTotal > TE->scores[0] || TE == nullptr)
+				TE = &TEs[name];
+		}
 	}
+
+	cout << "QB: " << QB->name << endl;			// <- for testing
+	cout << "RB1: " << RB1->name << endl;
+	cout << "RB2: " << RB2->name << endl;
+	cout << "WR1: " << WR1->name << endl;
+	cout << "WR2: " << WR2->name << endl;
+	cout << "TE: " << TE->name << endl;
+
 	// open weekly files
-	for () {
+	/**for () {
 		// create maxHeap of Player* for each position
 		// for each maxHeap
 			// calculate top's complement values, remove from heap until top has 0 complement value
 	}
 	QBc = findComplement(QBs, QB);
 	TEc = findComplement(TEs, TE);
-	DSTc = findComplement(DSTs, DST);
-	Kc = findComplement(Ks, K);
 
-	printStats();
+	printStats();*/
 }
